e69bbfac13692c25e5519734ca084fa404804858
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-18 16:47:19 +0300

    create new componenet

--- Quotegram/src/app/app.module.ts
+++ Quotegram/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
 @NgModule({
   declarations: [
     AppComponent,
+    QuotesComponent,
   ],
   imports: [
     BrowserModule,
