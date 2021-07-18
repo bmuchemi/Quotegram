5740389cc6a3b0b04145f1227d4a4998796a90ee
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-18 16:09:52 +0300

    add base stylings to the app

--- Quotegram/src/app/app.module.ts
+++ Quotegram/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
