8704ff05751193ea9f767b7fe7e77bdaf9409513
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-18 19:49:18 +0300

    add objects to calculate time

--- Quotegram/src/app/time-count.pipe.ts
+++ Quotegram/src/app/time-count.pipe.ts
@@ -5,8 +5,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): string {
+    const dayInSeconds = 86400;
+    let today:Date = new Date();
+    let todayWithoutTime:any = new Date (today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDifference = Math.abs(todayWithoutTime - value);
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var daysCount = dateDifferenceSeconds/dayInSeconds; // number of days
+    var days = `${daysCount} days ago`;
+    var monthsCount = Math.round(daysCount/30);
+    var months = `${monthsCount} months ago`;
+    var yearsCount = Math.round(monthsCount/12);
+    var years = `${yearsCount} years ago`;
+
+    if(value <= today){
+      if (daysCount < 30){
+        return days;
+      }else if (daysCount >= 30){
+        if (daysCount <= 365){
+          return months;
+        } else{
+          return years;
+        }
+      }
+    }else{
+      return ``;
+    }
   }
 
 }
