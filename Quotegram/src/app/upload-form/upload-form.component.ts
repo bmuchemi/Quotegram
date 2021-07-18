042cc8c8fbabaa2075c8c1e711e38feeac3894fc
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-18 19:20:11 +0300

    add formsmodule to app.module.ts

--- Quotegram/src/app/upload-form/upload-form.component.ts
+++ Quotegram/src/app/upload-form/upload-form.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFormComponent implements OnInit {
 
+  newQuote = new Quotes (0,"","","",new Date());
+  @Output () addQuote = new EventEmitter<Quotes>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
