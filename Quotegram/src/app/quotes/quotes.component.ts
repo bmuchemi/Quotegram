5281db19869f765622a29043071cdd62c04243e9
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-19 14:37:25 +0300

    add stylings to the quotes component

--- Quotegram/src/app/quotes/quotes.component.ts
+++ Quotegram/src/app/quotes/quotes.component.ts
@@ -10,7 +10,7 @@ export class QuotesComponent implements OnInit {
   quotes: Quotes[]=[
     new Quotes(1, 'I came, I saw, I conquered.', 'Julias Caeser', 'Benjamin ', new Date(2021,7,17)),
     new Quotes(2, 'Survival isnt who you are, it is who you become.', 'The 100', 'Carol', new Date(2021,6,18)),
-    new Quotes(3, 'Your time is limited, so dont waste it living someone elses life. Dont be trapped by dogma – which is living with the results of other peoples thinking.', 'Steve Jobs', 'Anonymous', new Date(2021,6,19))
+    new Quotes(3, 'Your time is limited, so dont waste it living someone elses life. Dont be trapped by dogma – which is living with the results of other peoples thinking.', 'Biston', 'Anonymous', new Date(2021,6,19))
   ];
 
   details(i:| number){
